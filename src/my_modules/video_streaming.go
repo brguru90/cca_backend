19b8749d49c0887a5900bea8dbc66772e0f313b9
Guruprasad BR <brguru90@gmail.com>
2023-05-11 14:25:26 +0530

    checking video contain audio before processing

--- src/my_modules/video_streaming.go
+++ src/my_modules/video_streaming.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"os"
 	"os/exec"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
@@ -52,6 +53,7 @@ func CreateHLS(inputFile string, outputDir string, segmentDuration int) error {
 	// 	"-f", "hls",
 	// 	fmt.Sprintf("%s/playlist.m3u8", outputDir),
 	// )
+
 	ffmpegCmd := exec.Command(
 		"ffmpeg",
 		"-i", inputFile,
@@ -72,7 +74,35 @@ func CreateHLS(inputFile string, outputDir string, segmentDuration int) error {
 		fmt.Sprintf("%s/playlist_%%v/manifest.m3u8", outputDir),
 	)
 
-	log.Debugln(ffmpegCmd.String())
+	{
+		ffprobeCmd := exec.Command(
+			"ffprobe",
+			"-i", inputFile,
+			"-show_streams",
+		)
+		output, err := ffprobeCmd.CombinedOutput()
+
+		if err != nil {
+			return err
+		}
+		if err == nil && !strings.Contains(string(output), "Stream #0:1") {
+			ffmpegCmd = exec.Command(
+				"ffmpeg",
+				"-i", inputFile,
+				"-filter_complex", `[0:v]split=4[v1][v2][v3][v4]; [v1]copy[v1out]; [v2]scale=w=1280:h=720:force_original_aspect_ratio=decrease[v2out]; [v3]scale=w=640:h=360:force_original_aspect_ratio=decrease[v3out]; [v4]scale=w=360:h=128:force_original_aspect_ratio=decrease[v4out]`,
+				"-map", "[v1out]", "-c:v:0", "libx264", "-b:v:0", "5M", "-maxrate:v:0", "5M", "-bufsize:v:0", "10M", "-preset", "medium", "-g", "48", "-sc_threshold", "0", "-keyint_min", "48",
+				"-map", "[v2out]", "-c:v:1", "libx264", "-b:v:1", "3M", "-maxrate:v:1", "3M", "-bufsize:v:1", "3M", "-preset", "medium", "-g", "48", "-sc_threshold", "0", "-keyint_min", "48",
+				"-map", "[v3out]", "-c:v:2", "libx264", "-b:v:2", "1M", "-maxrate:v:2", "1M", "-bufsize:v:2", "1M", "-preset", "medium", "-g", "48", "-sc_threshold", "0", "-keyint_min", "48",
+				"-map", "[v4out]", "-c:v:3", "libx264", "-b:v:3", "0.5M", "-maxrate:v:3", "0.5M", "-bufsize:v:3", "0.5M", "-preset", "medium", "-g", "48", "-sc_threshold", "0", "-keyint_min", "48",
+				"-f", "hls", "-hls_time", "2", "-hls_playlist_type", "vod",
+				"-hls_flags", "independent_segments", "-hls_segment_type", "mpegts",
+				"-hls_segment_filename", fmt.Sprintf("%s/playlist_%%v/data%%02d.ts", outputDir),
+				"-master_pl_name", "playlist.m3u8",
+				"-var_stream_map", "v:0 v:1 v:2 v:3",
+				fmt.Sprintf("%s/playlist_%%v/manifest.m3u8", outputDir),
+			)
+		}
+	}
 
 	output, err := ffmpegCmd.CombinedOutput()
 	log.Debugf("failed to create HLS: %v\nOutput: %s", err, string(output))
