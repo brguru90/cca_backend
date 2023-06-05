cf5a6e1783bb90beb7585e4bd4afdca04ed26948
Guruprasad BR <brguru90@gmail.com>
2023-06-05 19:14:28 +0530

    .

--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
 sudo -E bash -c "gcsfuse  ccast /home/sathyanitsme/cdn"
 sudo -E bash -c "gcsfuse  cca-private /home/sathyanitsme/storage"
 ./go_server
-
-sudo mount -t gcsfuse -o rw,allow_other  ccast "/home/sathyanitsme/cdn"
