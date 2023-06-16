81960180eda07906a0c6727c6208bad620c91fcd
Guruprasad BR <brguru90@gmail.com>
2023-06-16 19:36:27 +0530

    .

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,12 +4,13 @@ module.exports = {
             name: "api_server",
             "script": "./go_server",
             "exec_interpreter": "none",
-            watch: false,   
+            watch: false,
             exec_mode: "fork_mode",
+            instances: 1,
             env_pm2: {
                 "NODE_ENV": "production"
             }
         },
-        
+
     ]
 }
