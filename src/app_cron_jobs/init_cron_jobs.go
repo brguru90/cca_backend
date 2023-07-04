acabd4536976a73814c2dc3c4675f7c3d8165356
Guruprasad BR <brguru90@gmail.com>
2023-07-04 16:25:33 +0530

    vm optimisation

--- src/app_cron_jobs/init_cron_jobs.go
+++ src/app_cron_jobs/init_cron_jobs.go
@@ -1,12 +1,18 @@
 package app_cron_jobs
 
 import (
+	"cca/src/database/database_connections"
+	"context"
+
 	"github.com/robfig/cron/v3"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 var CRON_JOBS *cron.Cron
 
 func InitCronJobs() {
+
+	database_connections.MONGO_COLLECTIONS.VideoStreamGenerationQ.DeleteMany(context.Background(), bson.M{})
 	CRON_JOBS = cron.New()
 	CRON_JOBS.AddFunc("*/15 * * * *", ClearExpiredToken)
 	CRON_JOBS.AddFunc("*/1 * * * *", VideoStreamGenerationCron)
