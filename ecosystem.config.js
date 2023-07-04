1855e212aa821fbabbda9191fe37a5ef88615869
Guruprasad BR <brguru90@gmail.com>
2023-07-04 13:40:45 +0530

    vm optimisation

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
         {
             name: "api_server",
-            "script": "./go_server",
+            "script": process.env?.IS_VIDEO_PROCESSING!="true"?"./go_server":"./go_server -micro_service video_processing",
             "exec_interpreter": "none",
             watch: false,
             exec_mode: "fork_mode",
